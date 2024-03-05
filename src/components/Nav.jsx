2b15408071c2abd3c3ad3ac573878effdc0cbb5c
jjacque1 <jacksonjacque.jj@gmail.com>
2024-03-05 11:36:26 -0500

    Nav completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Librarylogo from '../assets/Library.svg'
 
 const Nav = () => {
   return (
     <nav>
         <div className="nav__container">
             <a href="/">
-                <img src="" alt="" className='logo'/>
+                <img src={Librarylogo} alt="" className='logo'/>
             </a>
             <ul className="nav__links">
                 <li className="nav__list">
@@ -19,9 +21,37 @@ const Nav = () => {
                     </a>
                 </li>
                 <button className='btn__menu'>
-                   
+                   <FontAwesomeIcon icon="bars"/>
                 </button>
+                <li className='nav__icon'>
+                    <a href="/cart" className="nav__link">
+                        <FontAwesomeIcon icon="shopping-cart"/>
+                    </a>
+                    <span className="cart__length">1</span>
+                </li>
             </ul>
+            <div className="menu__backdrop">
+                <button className="btn__menu btn__menu--close">
+                    <FontAwesomeIcon icon="times"/>
+                </button>
+                <ul className="menu__links">
+                    <li className="menu__list">
+                        <a href="/" className="menu__link">
+                            Home
+                        </a>
+                    </li>
+                    <li className="menu__list">
+                        <a href="/books" className="menu__link">
+                            Books
+                        </a>
+                    </li>
+                    <li className="menu__list">
+                        <a href="/cart" className="menu__link">
+                            Cart
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
   )
