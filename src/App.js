16a96bc4cae66279f17ff851288f28ff629b3b94
jjacque1 <jacksonjacque.jj@gmail.com>
2024-03-23 16:40:01 -0400

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted/>
-      <Explore/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
